3d7fafe42e996ee51564fcb1110c55314bdf15fb
Iain J. Reid <iainreid382@gmail.com>
2021-06-23 16:09:19 +0000

    feat: Add dependencies-update action

---
+++ dependencies-update/dependencies-update.sh
@@ -0,0 +1,41 @@
+#!/bin/sh -e
+
+DEPENDENCIES_SCOPE=$1
+GIT_COMMIT_MESSAGE=$2
+GIT_COMMIT_BRANCH=$3
+
+# Check for updates to @orbiitai dependencies
+yarn upgrade --scope "$DEPENDENCIES_SCOPE" --latest
+
+# Run an audit on the new/existing dependencies
+yarn audit
+
+if git diff-index --quiet HEAD 2>&1; then
+  echo "No changes... Bye!"
+  exit
+fi
+
+# Create a branch on which to commit the changes using the "actions" namespace
+# (please, nobody commit to this branch, you might loose your changes).
+git checkout -b "$GIT_COMMIT_BRANCH"
+
+# Commit the changes with a simple message
+git commit -am "$GIT_COMMIT_MESSAGE"
+
+# Push the branch to GitHub (we're using -f here to make that if there's a
+# pending PR open against this branch, it'll just be updated with the latest
+# changes).
+git push -u origin "$(git rev-parse --abbrev-ref HEAD)" -f
+
+if ! command -v gh 2>&1 /dev/null; then
+  echo "GitHub CLI not found. Please create the PR manually."
+  exit
+fi
+
+# Create a PR from this branch (in the event that there is already a PR open
+# against this branch, this command should exit gracefully).
+gh pr create \
+  --title "$GIT_COMMIT_MESSAGE" \
+  --body "This is an automated dependency update, review me!" \
+  --label dependencies
+
